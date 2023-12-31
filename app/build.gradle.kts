81d8bb36e645d70f2c00970e7cf59fb157d0621f
Josue Vicente <josue.vicente@ufl.edu>
2023-12-31 08:24:21 -0500

    Fixed Navigation Bug
    build.gradle.kts updated to include viewmodel compose version 2.6.2
    HomeScreen.kt  updated homescreen to use viewModel()
    
    Current update: 2 modes, weekly and montly.
    User is able to switch between them.
    Weekly: 2 events are displayed per day
    Montly: the days with events are highlighted.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
