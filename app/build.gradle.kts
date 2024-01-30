d8e60124a2d43be95fa9c276d18b87ce1865a567
Anthony Zurita <anthonyy.zurita@gmail.com>
2024-01-29 22:01:52 -0500

    GraphQL set up and plugins downloaded

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.apollographql.apollo3").version("4.0.0-beta.4")
+
 }
 
 android {
@@ -62,10 +64,19 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended:${rootProject.extra["compose_version"]}")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("junit:junit:4.12")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("com.apollographql.apollo3:apollo-runtime:4.0.0-beta.4")
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.example.shpe_uf_mobile_kotlin")
+    }
 }
---
+++ app/src/main/java/Apollo.kt
@@ -0,0 +1,5 @@
+import com.apollographql.apollo3.ApolloClient
+
+val apolloClient = ApolloClient.Builder()
+    .serverUrl("https://5cc1-153-33-119-21.ngrok-free.app/")
+    .build()
