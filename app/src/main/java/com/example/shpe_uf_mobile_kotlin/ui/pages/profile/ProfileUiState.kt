3e2b46e658f9bf2ea4878ec848c1d386fcbb33e3
adrianp-7 <aipavila7@gmail.com>
2024-02-29 17:55:59 -0500

    Wrote skeleton code for ViewModel and UIState file

---
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/profile/ProfileUiState.kt
@@ -0,0 +1,6 @@
+package com.example.shpe_uf_mobile_kotlin.ui.pages.profile
+
+data class ProfileUiState(
+    //TODO: figure out how to set currentName to be user input from ProfilePage.kt file
+    val currentName:String = "",
+)
