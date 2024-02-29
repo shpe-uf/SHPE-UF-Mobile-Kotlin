34895c442e39722284aba805bc71623818bc4baa
Andrei Ursu <u.andrei678@gmail.com>
2024-02-28 19:09:46 -0500

    Working on importing from FIGMA, now trying to add text. Note the gator is being laid over the orange and white backgrounds.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/SignInScreen.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/SignInScreen.kt
@@ -1,12 +1,15 @@
 package com.example.shpe_uf_mobile_kotlin.ui.pages.signIn
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -17,42 +20,81 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.shpe_uf_mobile_kotlin.R
 import com.example.shpe_uf_mobile_kotlin.ui.theme.OrangeSHPE
 import com.example.shpe_uf_mobile_kotlin.ui.theme.WhiteSHPE
 
 @Preview
 @Composable
 fun SignInUIPreview(){
+    SignInBackground()
     SignInLayout()
 }
 
 @Composable
-fun SignInLayout(){
+fun SignInBackground(modifier: Modifier = Modifier) {
+    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    val orangeHeight = screenHeight * (1.01f / 11f)
+    val whiteHeight = screenHeight * (10f / 11f)
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(orangeHeight)
+                .background(OrangeSHPE)
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(whiteHeight)
+                .align(Alignment.BottomCenter)
+                .background(WhiteSHPE)
+        )
+    }
+
     Column(
-        modifier = Modifier
-            .padding(horizontal = 15.dp, vertical = 30.dp)
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(50.dp)
-    ){
-        Box(
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.gatorshpe),
+            contentDescription = "SHPE GATOR",
             modifier = Modifier
-                .width(393.dp)
-                .height(93.dp)
-                .background(color = OrangeSHPE)
+                .offset(y=-10.dp)
         )
-        Box(
-            modifier = Modifier
-                .width(515.dp)
-                .height(809.dp)
-                .background(color = WhiteSHPE)
-        )
+    }
+}
+
+@Composable
+fun SignInLayout(modifier: Modifier = Modifier){
+    Column {
         Text(
-            text= "Sign In."
+            text = "SIGN IN",
+            style = TextStyle(
+                fontSize = 50.sp,
+                fontFamily = FontFamily.Cursive,
+                fontWeight = FontWeight(400),
+                color = OrangeSHPE,
+            ),
+            modifier = Modifier
+                .width(180.dp)
+                .height(42.dp)
         )
     }
 }
