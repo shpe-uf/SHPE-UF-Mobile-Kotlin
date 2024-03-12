1ee262669e63b084dc79db6e0bf693b6cad407f4
Andrei Ursu <u.andrei678@gmail.com>
2024-03-12 11:58:37 -0400

    Added Apollo root file.

---
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
@@ -0,0 +1,6 @@
+import com.apollographql.apollo3.ApolloClient
+
+val apolloClient = ApolloClient.Builder()
+
+    .serverUrl("https://7f5f-2600-1700-ee10-89a0-3590-35e8-7aec-c234.ngrok-free.app/")
+    .build()
