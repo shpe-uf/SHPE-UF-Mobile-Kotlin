93c24ae45fd35f57f2bedad85cf6005508038148
Andrei Ursu <u.andrei678@gmail.com>
2024-03-12 16:21:31 -0400

    Removed space.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
@@ -1,6 +1,5 @@
 import com.apollographql.apollo3.ApolloClient
 
 val apolloClient = ApolloClient.Builder()
-
     .serverUrl("https://7f5f-2600-1700-ee10-89a0-3590-35e8-7aec-c234.ngrok-free.app/")
     .build()
