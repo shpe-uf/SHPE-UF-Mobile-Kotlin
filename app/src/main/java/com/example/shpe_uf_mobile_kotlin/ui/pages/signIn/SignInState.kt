7e9088841cbfcc509876662b00a406a9ba843d1a
Andrei Ursu <u.andrei678@gmail.com>
2024-03-12 12:06:35 -0400

    Updated data class to have the username replace the email field.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/SignInState.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/SignInState.kt
@@ -1,10 +1,10 @@
 package com.example.shpe_uf_mobile_kotlin.ui.pages.signIn
 
 data class SignInState(
-    val email: String? = null,
+    val username: String? = null,
     val password: String? = null,
 
-    val emailErrorMessage: String? = null,
+    val usernameErrorMessage: String? = null,
     val passwordErrorMessage: String? = null,
 
     val isPasswordVisible: Boolean = true
