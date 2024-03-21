be38d20d57ea90b72e4810ca00938deab8a4f0ac
Andrei Ursu <u.andrei678@gmail.com>
2024-03-21 17:30:12 -0400

    Changed url.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
@@ -1,5 +1,5 @@
 import com.apollographql.apollo3.ApolloClient
 
 val apolloClient = ApolloClient.Builder()
-    .serverUrl("https://7f5f-2600-1700-ee10-89a0-3590-35e8-7aec-c234.ngrok-free.app/")
+    .serverUrl("https://765f-128-227-1-36.ngrok-free.app/")
     .build()
