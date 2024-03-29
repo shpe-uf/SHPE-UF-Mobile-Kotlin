56898221929e030f6245a0bb3456bb1e86239dde
TheAndreiUrsu <u.andrei678@gmail.com>
2024-03-29 12:19:49 -0400

    Added opening page state that contains a list of drawables.

---
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/opening/OpeningPageState.kt
@@ -0,0 +1,16 @@
+package com.example.shpe_uf_mobile_kotlin.ui.pages.opening
+
+import com.example.shpe_uf_mobile_kotlin.R
+
+data class OpeningPageState (
+    val images: List<Int> = listOf(
+        R.drawable.opening_1,
+        R.drawable.opening_2,
+        R.drawable.opening_3,
+        R.drawable.opening_4,
+        R.drawable.opening_5,
+        R.drawable.opening_6,
+        R.drawable.opening_7,
+        R.drawable.opening_8,
+    )
+)
