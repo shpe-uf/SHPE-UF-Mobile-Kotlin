bcd40907ec6732d879f07b329f0de5bdbd0b9b7b
TheAndreiUrsu <u.andrei678@gmail.com>
2024-03-29 12:20:15 -0400

    Created a viewmodel to keep track of which image is on the page and to eventually take in input with the button.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/opening/OpeningViewModel.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/opening/OpeningViewModel.kt
@@ -1,1 +1,13 @@
 package com.example.shpe_uf_mobile_kotlin.ui.pages.opening;
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+
+class OpeningViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(OpeningPageState())
+    val uiState: StateFlow<OpeningPageState> = _uiState.asStateFlow()
+}
