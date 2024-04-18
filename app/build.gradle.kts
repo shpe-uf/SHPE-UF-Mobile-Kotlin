4bb776d4b2d728c8534372d4192e4020ea61471a
Josue Vicente <josue.vicente@ufl.edu>
2024-04-17 20:11:59 -0400

    Quick Navigation Bar Update
    The UI was updated to move the bottom navigation bar upwards if there was a display of navigation buttons.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material:1.6.2")
     implementation("com.google.android.material:material:1.11.0")
