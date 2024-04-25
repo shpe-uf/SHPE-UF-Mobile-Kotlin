6af1ea941274970c640f5c3ee2ae959033853f9a
Josue Vicente <josue.vicente@ufl.edu>
2024-04-25 19:37:37 -0400

    Update to build.gradles
    Added rooms for local event storage but required Java 17 so i changed that but I am unsure how this will affect the rest of the app for now.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ import java.io.FileInputStream
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -45,11 +46,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         buildConfig = true
@@ -67,6 +68,10 @@ android {
 }
 
 dependencies {
+    implementation("androidx.room:room-runtime:2.4.0")
+    implementation("androidx.room:room-ktx:2.4.0")
+    ksp( "androidx.room:room-compiler:2.5.0")
+
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material:1.6.2")
