5abc5e57facfc237c8059ca4cbb6b7d095df7e84
Andrei Ursu <u.andrei678@gmail.com>
2024-05-13 13:34:20 -0400

    Added 2 dependecies for lerp() function.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,5 +68,11 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Might remove in the future.
     implementation ("com.google.accompanist:accompanist-pager:0.27.1")
+
+    // Used for Jetpack Compose's new Paging library.
+    implementation("androidx.compose.ui:ui-util:$version")
+    implementation("androidx.compose.ui:ui:$version")
 }
