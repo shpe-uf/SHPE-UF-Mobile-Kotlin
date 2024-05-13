b94dd9b478860958540eac21b1feaa2f5c968561
Andrei Ursu <u.andrei678@gmail.com>
2024-05-13 13:34:48 -0400

    Reworked image carousel to work with Jetpack Compose's pager instead of the depreciated accompanist pager.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/opening/Opening.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/opening/Opening.kt
@@ -1,18 +1,14 @@
 package com.example.shpe_uf_mobile_kotlin.ui.pages.opening
 
 import android.util.Log
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.TweenSpec
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.collectIsDraggedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,21 +19,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -49,15 +35,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.shpe_uf_mobile_kotlin.R
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.util.lerp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import kotlin.math.absoluteValue
 
 // The goat source: https://blog.protein.tech/jetpack-compose-auto-image-slider-with-dots-indicator-45dfeba37712
 
-@OptIn(ExperimentalPagerApi::class, ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class)
 @Preview
 @Composable
 fun OpeningPage() {
@@ -85,68 +76,112 @@ fun OpeningPage() {
     )
     // TODO: Add to ViewModel.
     val pagerState = rememberPagerState(
-        initialPage = 0
+        pageCount = {subText.size}
     )
-    // TODO: Add to ViewModel.
-    var visible by remember { mutableStateOf(true) }
-    // TODO: Add to ViewModel.
-    val isDragged by pagerState.interactionSource.collectIsDraggedAsState() // to prevent auto scrolling when dragging the screen
 
-    HorizontalPager(
-        count = images.size,
-        state = pagerState,
-        modifier = Modifier.fillMaxSize()
-    ) { currentPage ->
-        // Text, image will change based on the current page.
-        // Offset each page to match figma design
-        Log.d("Page", ""+currentPage)
-        Image(
-            painter = painterResource(id = images[currentPage]),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
+    // Start Pager
+    HorizontalPager(state = pagerState) { page ->
+        Card(
+            Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(),
-            alpha = 0.5f
-        )
+                .fillMaxHeight()
+                .graphicsLayer{
+                    val pageOffset = (
+                            (pagerState.currentPage - page) + pagerState
+                                .currentPageOffsetFraction
+                            ).absoluteValue
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            AnimatedVisibility(
-                visible = visible,
-                enter = fadeIn(animationSpec = tween(durationMillis = 1500)),
-                exit = fadeOut(animationSpec = tween(durationMillis = 2000))
-            ) {
-                Text(
-                    text = subText[currentPage],
-                    style = TextStyle(
-                        fontSize = 25.sp,
-                        fontFamily = FontFamily.Serif,
-                        fontWeight = FontWeight(700),
-                        color = Color(0xFFB0B0B0),
-                    ),
-                    modifier = Modifier
-                        .offset(y = 84.dp) // TODO: Fix spacing between SHPE UF and the sub text.
-                        .height(40.dp)
-                )
-            }
+                    // We animate the alpha, between 50% and 100%
+                    alpha = lerp(
+                        start = 0.5f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    )
+                }
+        ){
+            Text(
+                text = subText[page],
+                style = TextStyle(
+                    fontSize = 25.sp,
+                    fontFamily = FontFamily.Serif,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFFB0B0B0),
+                ),
+            )
+            Image(
+                painter = painterResource(id = images[page]),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                //alpha = 0.5f
+            )
+
+
 
         }
-        EllipseBar(selectedIndex = if (isDragged) pagerState.currentPage else pagerState.currentPage)
+
+    } // end pager
+
+    Row(
+        Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .align(Alignment.BottomCenter)
+            .padding(bottom = 8.dp),
+        horizontalArrangement = Arrangement.Center
+    ){
+
     }
 
-    with(pagerState){
-        LaunchedEffect(key1 = currentPage){
-            launch{
-                delay(2500)
-                val nextPage = (pagerState.currentPage + 1) % pagerState.pageCount
-                pagerState.animateScrollToPage(page = nextPage, pageOffset = 0.0f)
-            }
-        }
-    }
+//    LaunchedEffect(Unit) {
+//        while (true) {
+//            //delay(2500)
+//            Log.d("async", "currently changing pages")
+//            val nextPage = (pagerState.currentPage + 1) % pagerState.pageCount
+//            pagerState.animateScrollToPage(nextPage)
+//        }
+//    }
+//
+//        HorizontalPager(
+//            count = images.size,
+//            state = pagerState,
+//            modifier = Modifier.fillMaxSize()
+//        ) { currentPage ->
+//            // Text, image will change based on the current page.
+//            // Offset each page to match figma design
+//            Log.d("Page", "Current page: "+currentPage)
+//            Image(
+//                painter = painterResource(id = images[currentPage]),
+//                contentDescription = null,
+//                contentScale = ContentScale.Crop,
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .fillMaxHeight(),
+//                alpha = 0.5f
+//            )
+//
+//            Column(
+//                modifier = Modifier.fillMaxSize(),
+//                verticalArrangement = Arrangement.Center,
+//                horizontalAlignment = Alignment.CenterHorizontally
+//            ) {
+//                    Text(
+//                        text = subText[currentPage],
+//                        style = TextStyle(
+//                            fontSize = 25.sp,
+//                            fontFamily = FontFamily.Serif,
+//                            fontWeight = FontWeight(700),
+//                            color = Color(0xFFB0B0B0),
+//                        ),
+//                        modifier = Modifier
+//                            .offset(y = 84.dp) // TODO: Fix spacing between SHPE UF and the sub text.
+//                            .height(40.dp)
+//                    )
+//            }
+//            //EllipseBar(selectedIndex = if (isDragged) pagerState.currentPage else pagerState.currentPage)
+//        }
 
     // SHPE Text
     Column(
@@ -176,7 +211,6 @@ fun OpeningPage() {
                 .offset(x = 5.dp, y = 21.dp)
                 .width(203.dp)
                 .height(58.dp)
-                .shadow(10.dp)
         )
     }
 
@@ -216,34 +250,14 @@ fun GettingStartedBtn(onClick: () -> Unit) { // TODO: Implement navigation to Lo
     }
 }
 
-// Creates a row of Dots and determines its color based on the current page displayed.
-@Composable
-fun EllipseBar(
-    selectedIndex: Int
-){
-    Box(modifier = Modifier.fillMaxSize()){
-        LazyRow(modifier = Modifier
-            .wrapContentHeight()
-            .wrapContentWidth()
-            .align(Alignment.BottomCenter)
-            .offset(y=(-5).dp), // taken from figma
-        ){
-            items(8) {index ->
-                Dot(color = if (index == selectedIndex) Color.White else Color(0xFF4c4c4c))
-
-                if(index != 7){
-                    Spacer(modifier = Modifier.padding(horizontal = 5.dp))
-                }
-            }
-        }
-    }
-
-}
-
 // Represents a dot that can either have a white or gray color. White = selected, gray = unselected.
 @Composable
-fun Dot(color: Color){
-    Canvas(modifier = Modifier.size(10.dp), onDraw ={
-        drawCircle(color = color)
-    })
+fun IndicatorDot(color: Color){
+    Box(
+        modifier = Modifier
+            .padding(2.dp)
+            .clip(CircleShape)
+            .background(color)
+            .size(16.dp)
+    )
 }
