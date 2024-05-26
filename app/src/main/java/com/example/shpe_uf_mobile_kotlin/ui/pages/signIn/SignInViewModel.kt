2c32cf5603ca294112e28152006703a642522c0d
FrancoiseHayek <hayekfreelancing@gmail.com>
2024-05-25 23:17:47 -0700

    Fixed issue with view model resetting after each input from the user

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/SignInViewModel.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/SignInViewModel.kt
@@ -1,32 +1,20 @@
 package com.example.shpe_uf_mobile_kotlin.ui.pages.signIn
 
 import android.util.Log
-import android.widget.Toast
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import apolloClient
-import com.apollographql.apollo3.exception.ApolloException
 import com.example.shpe_uf_mobile_kotlin.LoginMutation
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-class SignInViewModel: ViewModel() {
+class SignInViewModel : ViewModel() {
 
-    // Mutable State Flow that holds the state of the Login Screen View.
     private val _uiState = MutableStateFlow(SignInState())
-    val uiState: StateFlow<SignInState> = _uiState.asStateFlow()
+    val uiState: StateFlow<SignInState> = _uiState
 
-    // Function to validate and login the user.
-    fun validateAndLoginUser(){
+    fun validateAndLoginUser() {
         val currentState = _uiState.value
 
         // Validate user input fields.
@@ -40,69 +28,60 @@ class SignInViewModel: ViewModel() {
         )
 
         // Login user if validations passed.
-        if(currentState.usernameErrorMessage == null && currentState.passwordErrorMessage == null){
-            Log.d("Validating", currentState.username.toString() + " | " + currentState.password.toString())
-            performLogin(currentState.username.toString(),currentState.password.toString())
-        }
-        else
+        if (currentState.usernameErrorMessage == null && currentState.passwordErrorMessage == null) {
+            Log.d("Validating", "${currentState.username} | ${currentState.password}")
+            performLogin(currentState.username.toString(), currentState.password.toString())
+        } else {
             Log.d("Validating", "Failure!")
-    }
-
-
-
-    private fun performLogin(username: String, password: String){
-        viewModelScope.launch {
-            val loginSuccess = loginUser(username,password)
-
-            if(!loginSuccess) updateErrorMessage("Could not login.") else updateErrorMessage("Logged in.")
-
         }
     }
 
-    // Handles user login using graphQL.
-    private suspend fun loginUser(username: String, password: String): Boolean{
-            // Mutation for logging in, returns the user's id on success.
-            val response = apolloClient.mutation(LoginMutation(username,password,"true")).execute()
+    private fun performLogin(username: String, password: String) {
+        viewModelScope.launch {
+            val loginSuccess = loginUser(username, password)
 
-            if(!response.hasErrors()){
-                val id = response.data?.login?.id
-                Log.d("GraphQL", "${id}")
-                return true
-            }
-            else{
-                Log.w("GraphQL", "Could not login.")
-                return false
-            }
+            if (!loginSuccess) updateErrorMessage("Could not login.") else updateErrorMessage("Logged in.")
+        }
     }
 
-    private fun updateErrorMessage(message: String){
-        val currentState = _uiState.value
+    private suspend fun loginUser(username: String, password: String): Boolean {
+        // Mutation for logging in, returns the user's id on success.
+        val response = apolloClient.mutation(LoginMutation(username, password, "true")).execute()
 
+        if (!response.hasErrors()) {
+            val id = response.data?.login?.id
+            Log.d("GraphQL", "$id")
+            return true
+        } else {
+            Log.w("GraphQL", "Could not login.")
+            return false
+        }
+    }
+
+    private fun updateErrorMessage(message: String) {
+        val currentState = _uiState.value
         _uiState.value = currentState.copy(
             loginErrorMessage = message
         )
-
     }
 
-    // Validates user inputs.
-    private fun validateUsername(username: String) : String?{
-        return if(username.isBlank()) "Username is required." else null
+    private fun validateUsername(username: String): String? {
+        return if (username.isBlank()) "Username is required." else null
     }
 
-    private fun validatePassword(password: String): String?{
-        return if(password.isBlank()) "Password is required." else null
+    private fun validatePassword(password: String): String? {
+        return if (password.isBlank()) "Password is required." else null
     }
 
-    // Functions to update the state for each input field.
-    fun onUsernameChanged(username: String){
-        _uiState.value = _uiState.value.copy(username = username)
+    fun onUsernameChanged(newUsername: String) {
+        _uiState.value = _uiState.value.copy(username = newUsername)
     }
 
-    fun onPasswordChanged(password: String){
-        _uiState.value = _uiState.value.copy(password = password)
+    fun onPasswordChanged(newPassword: String) {
+        _uiState.value = _uiState.value.copy(password = newPassword)
     }
 
-    fun togglePasswordVisibility(){
+    fun togglePasswordVisibility() {
         _uiState.value = _uiState.value.copy(isPasswordVisible = !_uiState.value.isPasswordVisible)
     }
 }
