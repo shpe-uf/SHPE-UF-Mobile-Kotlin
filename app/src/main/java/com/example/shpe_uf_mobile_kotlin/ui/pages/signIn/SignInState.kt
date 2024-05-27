b5c6dc852580a7a75bee991ffce3008225ea3d53
TheAndreiUrsu <u.andrei678@gmail.com>
2024-05-27 12:49:58 -0400

    Added comments describing the class variables. Tidied up the conventions.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/SignInState.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/SignInState.kt
@@ -1,13 +1,17 @@
 package com.example.shpe_uf_mobile_kotlin.ui.pages.signIn
 
 data class SignInState(
+
+    // By default, username and password are null.
     val username: String? = null,
     val password: String? = null,
 
-    val usernameErrorMessage: String? = null,
-    val passwordErrorMessage: String? = null,
-    val loginErrorMessage: String? = null,
+    // By default user should be unable to login.
+    val usernameErrorMessage: String? = "Username is required.",
+    val passwordErrorMessage: String? = "Password is required.",
+    val loginErrorMessage: String? = "Unable to login.",
 
-    val isPasswordVisible: Boolean = true
+    // By default password should be invisible.
+    val isPasswordVisible: Boolean = false
 )
 
