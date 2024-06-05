4b0f35e4ff48f01eae4e7f69e04fb4a9fee0117d
TheAndreiUrsu <u.andrei678@gmail.com>
2024-06-04 23:33:48 -0400

    Updated serverUrl.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
@@ -1,5 +1,5 @@
 import com.apollographql.apollo3.ApolloClient
 
 val apolloClient = ApolloClient.Builder()
-    .serverUrl("https://369f-2600-1700-ee10-89a0-8095-608c-54a2-472f.ngrok-free.app/")
+    .serverUrl("https://6517-2600-1700-ee10-89a0-9a4-6a09-d1d4-fa40.ngrok-free.app")
     .build()
