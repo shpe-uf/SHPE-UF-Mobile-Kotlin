f27c7198d93faa36e13454e8215c5173a1b63584
theandreiursu <u.andrei678@gmail.com>
2024-09-06 14:55:26 -0400

    Update MainActivity.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/MainActivity.kt
@@ -31,19 +31,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SHPEUFMobileKotlinTheme {
-        Greeting("Android")
-    }
-}
