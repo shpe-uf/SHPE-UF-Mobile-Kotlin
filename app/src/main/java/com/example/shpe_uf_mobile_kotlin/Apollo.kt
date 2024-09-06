a8eba6ffa5475c333262b12c96c61ca7be271d88
theandreiursu <u.andrei678@gmail.com>
2024-09-05 20:09:52 -0400

    Update syntax.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
@@ -3,6 +3,5 @@ package com.example.shpe_uf_mobile_kotlin
 import com.apollographql.apollo3.ApolloClient
 
 val apolloClient = ApolloClient.Builder()
-
     .serverUrl("https://cbab-128-227-38-193.ngrok-free.app")
     .build()
