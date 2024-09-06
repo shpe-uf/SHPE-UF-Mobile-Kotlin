a0886d4d014ff77f04827bffc79945d77d793666
theandreiursu <u.andrei678@gmail.com>
2024-09-05 21:04:39 -0400

    Added classes, internships, socialMedia, and photo attributes for the profile ui.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/profile/ProfileUiState.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/profile/ProfileUiState.kt
@@ -9,4 +9,8 @@ data class ProfileUiState(
     val country: String? = null,
     val year: String? = null,
     val gradYear: String? = null,
+    val classes: Array<String>? = null,
+    val internships: Array<String>? = null,
+    val socialMedia: Array<String>? = null,
+    val photo: String? = null
 )
