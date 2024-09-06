e406daa1cfcbe8f3e57433a17ca01f16e8533f84
theandreiursu <u.andrei678@gmail.com>
2024-09-05 20:42:06 -0400

    Added asserts for all the fields except for photo.

--- app/src/test/java/com/example/shpe_uf_mobile_kotlin/profile_tests/ProfileQueryTests.kt
+++ app/src/test/java/com/example/shpe_uf_mobile_kotlin/profile_tests/ProfileQueryTests.kt
@@ -11,6 +11,23 @@ private suspend fun getUserInfo(id: String): Boolean{
 
     if(!response.hasErrors()){
         val result = response.data?.getUser
+
+        if(result != null){
+            val name = result.firstName + " " + result.lastName
+
+            assertEquals(name, "Andrei Ursu")
+            assertEquals(result.username, "andreiursu")
+            assertEquals(result.email, "ursuandrei@ufl.edu")
+            assertEquals(result.sex, "Male")
+            assertEquals(result.ethnicity, "White")
+            assertEquals(result.country, "United States")
+            assertEquals(result.year, "3rd Year")
+            assertEquals(result.graduating, "2025")
+            assertEquals(result.classes?.size, 0)
+            assertEquals(result.internships?.size, 0)
+            assertEquals(result.socialMedia?.size, 0)
+        }
+
         println(result)
         return true
     }
@@ -22,10 +39,8 @@ class ProfileQueryTests {
     // Testing profile query with SHPE ID.
     @Test
     fun testGetUser() = runTest{
-        val id = "64ee138ec736670014d47e48"
-
+        val id = "64ee138ec736670014d47e48" // Andrei's shpe id, use for testing purposes.
         val result = getUserInfo(id)
-
         assertEquals(result, true)
     }
 }
