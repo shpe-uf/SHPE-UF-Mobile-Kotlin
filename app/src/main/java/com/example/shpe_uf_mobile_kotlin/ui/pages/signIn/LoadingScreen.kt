9224bcac67b37ee5b0675c81d4f45af1107c490f
TheAndreiUrsu <u.andrei678@gmail.com>
2024-09-06 18:23:40 -0400

    Added loading screen that occurs after the opening page and before the login screen.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/LoadingScreen.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/signIn/LoadingScreen.kt
@@ -1,71 +1,109 @@
 package com.example.shpe_uf_mobile_kotlin.ui.pages.signIn
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.shpe_uf_mobile_kotlin.R
 import com.example.shpe_uf_mobile_kotlin.ui.theme.OrangeSHPE
-import com.example.shpe_uf_mobile_kotlin.ui.theme.ThemeColors
+import com.example.shpe_uf_mobile_kotlin.ui.theme.dark_bg
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun LoadingScreen(){
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(OrangeSHPE),
-    ){
-        Column(
+
+    // Dark mode support
+    val gator = if (isSystemInDarkTheme()){
+        painterResource(R.drawable.gator)
+    }
+    else {
+        painterResource(R.drawable.light_gator)
+    }
+
+    Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.align(Alignment.Center)
+            modifier = Modifier.background(OrangeSHPE).fillMaxSize()
         ) {
-            Image(painter = painterResource(R.drawable.gator),
+            Spacer(Modifier.height(204.dp))
+            Image(painter = gator,
                 contentDescription = "Gator",
                 contentScale = ContentScale.FillBounds,
                 modifier = Modifier
-                    .width(312.dp))
-            Spacer(modifier = Modifier.height(30.dp))
+                    .width(312.dp)
+                    .height(210.dp))
+            Spacer(Modifier.height(30.dp))
             Text(text = "SHPE UF",
                 style = TextStyle(
                     fontSize = 64.sp,
+                    fontFamily = FontFamily(Font(R.font.viga)),
                     fontWeight = FontWeight(400),
                     color = Color.White
                 )
             )
-            Spacer(modifier = Modifier.height(223.dp))
-            Box(modifier = Modifier
-                    .width(129.dp)
-                    .height(126.dp)
-                    .clip(CircleShape)
-                    .background(Color.White)) {
-                Image(painter = painterResource(R.drawable.shpe_logo_full_color),
-                    contentDescription = "SHPE Logo")
-            }
+            Spacer(Modifier.height(150.dp))
+            ButtonSHPE { TODO("Navigate to login screen.") }
 
-            }
         }
     }
+
+@Composable
+fun ButtonSHPE(onClick: @Composable () -> Unit){
+
+    val interactionSource = remember{ MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val color = if (isPressed) dark_bg else Color.White
+
+    Button(onClick = {onClick},
+        interactionSource = interactionSource,
+        modifier = Modifier
+            .width(129.dp)
+            .height(126.dp)
+            .clip(CircleShape),
+            //.padding(bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = color,
+        ),
+        elevation = ButtonDefaults.buttonElevation(4.dp)
+        ){
+            Image(painter = painterResource(R.drawable.shpe_logo_full_color),
+                contentDescription = "SHPE Logo",
+                modifier = Modifier.width(104.dp).height(112.dp))
+        }
+}
