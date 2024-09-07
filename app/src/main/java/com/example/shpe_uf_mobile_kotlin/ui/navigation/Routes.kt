82c0855254d42e36da2bc633c2975d752b73db67
TheAndreiUrsu <u.andrei678@gmail.com>
2024-09-06 20:58:13 -0400

    Added register to routes.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/navigation/Routes.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/navigation/Routes.kt
@@ -2,6 +2,7 @@ package com.example.shpe_uf_mobile_kotlin.ui.navigation
 
 object Routes {
     var login = "login"
+    var register = "register"
     var loading = "loading"
     var opening = "opening"
 }
