8fd4b61cb73e73d2766f551a2f22d921f2eafee8
theandreiursu <u.andrei678@gmail.com>
2024-09-08 18:06:24 -0400

    Added navigation for opening -> loading -> login.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.shpe_uf_mobile_kotlin.ui.navigation.Routes
+import com.example.shpe_uf_mobile_kotlin.ui.navigation.SHPEUFNavController
 import com.example.shpe_uf_mobile_kotlin.ui.pages.opening.OpeningPage
 import com.example.shpe_uf_mobile_kotlin.ui.pages.signIn.LoadingScreen
 import com.example.shpe_uf_mobile_kotlin.ui.pages.signIn.SignIn
@@ -30,21 +31,23 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    val pointsPageViewModel = PointsPageViewModel()
 //                    FullView(pointsPageViewModel = pointsPageViewModel)
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = Routes.opening,
-                        builder = {
-                            composable(Routes.loading) {
-                                LoadingScreen(navController)
-                            }
-                            composable(Routes.login) {
-                                SignIn()
-                            }
-                            composable(Routes.opening){
-                                OpeningPage(navController)
-                            }
-                        })
+//                    val navController = rememberNavController()
+//                    NavHost(
+//                        navController = navController,
+//                        startDestination = Routes.opening,
+//                        builder = {
+//                            composable(Routes.loading) {
+//                                LoadingScreen(navController)
+//                            }
+//                            composable(Routes.login) {
+//                                SignIn()
+//                            }
+//                            composable(Routes.opening){
+//                                OpeningPage(navController)
+//                            }
+//                        })
+                    val navController = SHPEUFNavController()
+                    navController.Navigation()
                 }
             }
         }
--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/navigation/SHPEUFNavController.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/navigation/SHPEUFNavController.kt
@@ -1,2 +1,33 @@
 package com.example.shpe_uf_mobile_kotlin.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.shpe_uf_mobile_kotlin.ui.pages.opening.OpeningPage
+import com.example.shpe_uf_mobile_kotlin.ui.pages.signIn.LoadingScreen
+import com.example.shpe_uf_mobile_kotlin.ui.pages.signIn.SignIn
+
+class SHPEUFNavController {
+
+    @Composable
+    fun Navigation(){
+        val navController = rememberNavController()
+
+        NavHost(
+            navController = navController,
+            startDestination = Routes.opening,
+            builder = {
+                composable(Routes.loading) {
+                    LoadingScreen(navController)
+                }
+                composable(Routes.login) {
+                    SignIn()
+                }
+                composable(Routes.opening){
+                    OpeningPage(navController)
+                }
+            })
+    }
+}
