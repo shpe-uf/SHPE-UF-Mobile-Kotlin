f40db0c8c6b08446dc023209cc78493d2edc1e74
theandreiursu <u.andrei678@gmail.com>
2024-09-09 18:26:14 -0400

    Added DataStore support to keep track of the user's id as a session.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/SHPEUFApp.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/SHPEUFApp.kt
@@ -4,11 +4,25 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.shpe_uf_mobile_kotlin.repository.UserRepository
 import com.example.shpe_uf_mobile_kotlin.util.NotificationsUtil
 
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "user_pref"
+)
+
 class SHPEUFApp : Application() {
+    // Adding data store stuff.
+    lateinit var userRepository: UserRepository
+
+
     override fun onCreate() {
         super.onCreate()
+        userRepository = UserRepository(dataStore)
         createNotificationChannel()
         NotificationsUtil.init(this)
     }
