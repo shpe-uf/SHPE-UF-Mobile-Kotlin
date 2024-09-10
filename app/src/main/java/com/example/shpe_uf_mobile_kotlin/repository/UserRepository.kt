bc575cfa74febbcbf6a36181dd7e83583bc502c5
theandreiursu <u.andrei678@gmail.com>
2024-09-09 21:59:11 -0400

    Added preferences to save, isLoggedIn, isLoggedOut, and DarkMode.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/repository/UserRepository.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/repository/UserRepository.kt
@@ -2,6 +2,7 @@ package com.example.shpe_uf_mobile_kotlin.repository
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.Flow
@@ -10,6 +11,9 @@ import kotlinx.coroutines.flow.map
 class UserRepository(private val dataStore: DataStore<Preferences>) {
     private companion object {
         val USER_ID = stringPreferencesKey("user_id")
+        val LOGGED_IN = booleanPreferencesKey("logged_in")
+        val LOGGED_OUT = booleanPreferencesKey("logged_out")
+        val DARK_MODE = booleanPreferencesKey("dark_mode") // true = dark mode, false = light mode
     }
 
     val currentUserId: Flow<String> = dataStore.data.map { preferences ->
@@ -21,4 +25,36 @@ class UserRepository(private val dataStore: DataStore<Preferences>) {
             preferences[USER_ID] = id
         }
     }
+
+    val currentLoggedIn: Flow<Boolean> = dataStore.data.map{ preferences ->
+        preferences[LOGGED_IN] ?: false
+    }
+
+    suspend fun saveLoggedIn(isLoggedIn: Boolean){
+        dataStore.edit{ preferences ->
+            preferences[LOGGED_IN] = isLoggedIn
+        }
+    }
+
+    val currentDarkMode: Flow<Boolean> = dataStore.data.map{ preferences ->
+        preferences[DARK_MODE] ?: false
+    }
+
+    suspend fun saveDarkMode(darkMode: Boolean){
+        dataStore.edit{ preferences ->
+            preferences[DARK_MODE] = darkMode
+        }
+    }
+
+    val currentLoggedOut: Flow<Boolean> = dataStore.data.map{ preferences ->
+        preferences[LOGGED_OUT] ?: false
+    }
+
+    suspend fun saveLoggedOut(isLoggedOut: Boolean){
+        dataStore.edit{ preferences ->
+            preferences[LOGGED_OUT] = isLoggedOut
+        }
+    }
+
+
 }
