020886e6d64eda4e6e65f5a59a132cc26d8579de
Anthony Zurita <anthonyy.zurita@gmail.com>
2024-09-11 14:26:48 -0400

    Cancel button implemented for profile page.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/profile/StaticProfilePage.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/profile/StaticProfilePage.kt
@@ -2,8 +2,6 @@ package com.example.shpe_uf_mobile_kotlin.ui.pages.profile
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -43,11 +40,11 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.shpe_uf_mobile_kotlin.R
+import com.example.shpe_uf_mobile_kotlin.data.SHPEUFAppViewModel
 import com.example.shpe_uf_mobile_kotlin.ui.navigation.NavRoute
 import com.example.shpe_uf_mobile_kotlin.ui.theme.SHPEUFMobileKotlinTheme
 
@@ -70,17 +67,28 @@ import com.example.shpe_uf_mobile_kotlin.ui.theme.SHPEUFMobileKotlinTheme
 
 //@Preview(showBackground = true)
 @Composable
-fun StaticProfilePagePreview(viewModel: ProfileViewModel, navController: NavHostController) {
+fun StaticProfilePagePreview(
+    viewModel: ProfileViewModel,
+    navController: NavHostController,
+    mainViewModel: SHPEUFAppViewModel
+) {
 
-    StaticProfileScreen(viewModel, navController)
+    StaticProfileScreen(viewModel, navController, mainViewModel)
 }
 
 
 @Composable
-fun StaticProfileScreen(profileViewModel: ProfileViewModel, navController: NavHostController){
+fun StaticProfileScreen(
+    profileViewModel: ProfileViewModel,
+    navController: NavHostController,
+    mainViewModel: SHPEUFAppViewModel
+){
     SHPEUFMobileKotlinTheme{
         val uiState by profileViewModel.uiState.collectAsState()
-        profileViewModel.loadProfile("64ea79b9f2051e00149c75b7")
+        val mainState by mainViewModel.uiState.collectAsState()
+
+
+        profileViewModel.loadProfile(mainState.id)
 
         StaticProfilePageBackground()
 
