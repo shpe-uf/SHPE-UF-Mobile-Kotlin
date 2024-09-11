898e366e6d48c77f9fcdfa75bc3ee6c0556ea591
Anthony Zurita <anthonyy.zurita@gmail.com>
2024-09-10 23:17:16 -0400

    Fixed year field and made it so it had a transparent background and trailing icon was changed to white to be consistent with Figma design.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/profile/ProfilePage.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/profile/ProfilePage.kt
@@ -445,7 +445,15 @@ fun ProfileYear(selectedYear: String, onYearSelected: (String) -> Unit, years: L
             readOnly = true,
             modifier = Modifier
                 .fillMaxWidth(0.8f)
-                .menuAnchor()
+                .menuAnchor(),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Gray,
+                unfocusedBorderColor = Color.Gray,
+                textColor = Color.White,
+                placeholderColor = Color.Gray,
+                focusedTrailingIconColor = Color.White,
+                unfocusedTrailingIconColor = Color.White
+            )
         )
         ExposedDropdownMenu(
             expanded = expanded,
