b251ad2fa298e6a42b0002126c5876c533133f65
TheAndreiUrsu <u.andrei678@gmail.com>
2025-02-26 15:37:39 -0500

    Updated Apollo.kt.

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/Apollo.kt
@@ -1,7 +1,8 @@
 package com.example.shpe_uf_mobile_kotlin
 
 import com.apollographql.apollo3.ApolloClient
+import com.example.shpe_uf_mobile_kotlin.BuildConfig.SERVER_URL
 
 val apolloClient = ApolloClient.Builder()
-    .serverUrl("https://274d-128-227-39-26.ngrok-free.app")
+    .serverUrl(SERVER_URL)
     .build()
