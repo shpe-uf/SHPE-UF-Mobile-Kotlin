4d265558b1a985b8b83fb1a97a86635d61578260
Julio A. Leonardi <jalg00065@gmail.com>
2025-10-08 17:14:00 -0400

    added reusable horizontal and vertical rolling banners

--- app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/wrapped/WrappedState.kt
+++ app/src/main/java/com/example/shpe_uf_mobile_kotlin/ui/pages/wrapped/WrappedState.kt
@@ -2,10 +2,10 @@ package com.example.shpe_uf_mobile_kotlin.ui.pages.wrapped
 
 data class WrappedState(
     val isLoading: Boolean = true,
-    val topMonth: String = "",
-    val topCategory: String = "",
-    val memberSince: String = "",
-    val semester: String = "",
+    val topMonth: String = "loading ...",
+    val topCategory: String = "loading ...",
+    val memberSince: String = "loading ...",
+    val semester: String = "loading ...",
     val points: Int = 0,
     val percentile: Int = 0,
     val error: String? = null
