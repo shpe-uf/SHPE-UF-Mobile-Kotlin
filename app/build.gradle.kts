d023f9ddcf9d96b23e37b75736e6a2e145f77605
adrianp-7 <aipavila7@gmail.com>
2026-01-29 12:11:43 -0500

    updated app versionCode and updated barcode dependencies to comply with 16KB alignment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.shpeuf.shpe_uf_mobile_kotlin"
         minSdk = 30
         targetSdk = 35
-        versionCode = 7
+        versionCode = 9
         versionName = "1.1"
 
         val localPropertiesFile = rootProject.file("local.properties")
@@ -81,7 +81,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
-    val camerax_version = "1.3.0"
+    val camerax_version = "1.4.0"
     implementation("androidx.camera:camera-camera2:$camerax_version")
     implementation("androidx.camera:camera-core:$camerax_version")
     implementation("androidx.camera:camera-lifecycle:$camerax_version")
@@ -92,8 +92,8 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.2.2")
 
     // ML Kit Barcode Scanning
-    implementation("com.google.mlkit:barcode-scanning:17.2.0")
-    implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.0.2")
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+    implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.1")
 
 
 
